3e61377badac2eafde803422f9f9f1c16fd85ec3
Felipe Dioguardi <felipedioguardi@gmail.com>
2020-12-27 00:05:55 -0300

    feat(polybar): disassociate polybar from the shell
    Add the "disown" function , as shown in polybar's documentation:
    https://github.com/polybar/polybar/wiki#launching-the-bar-in-your-wms-bootstrap-routine

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,16 +3,16 @@
 # Terminate already running bar instances
 killall -q polybar
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
 # Launch bar in every connected monitor
 bar=main
 
 echo "---" | tee -a /tmp/$bar.log
 
 for monitor in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$monitor polybar --reload $bar -c ~/.config/polybar/config.ini >> /tmp/$bar.log 2>&1 &
+    MONITOR=$monitor polybar \
+      --reload $bar \
+      -c ~/.config/polybar/config.ini \
+      >> /tmp/$bar.log 2>&1 & disown
 done
 
 echo "Bars launched..."
