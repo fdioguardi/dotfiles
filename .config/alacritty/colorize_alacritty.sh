3098c09793d1c3de0c269d9d489bd1d29e3013b6
Felipe Dioguardi <felipedioguardi@gmail.com>
2021-03-01 17:25:39 -0300

    refactor(recolorize): append colors to static file
    Have one file containing the Alacritty's settings, and another one with
    color placeholders. Use the script to simply merge those two.

--- .config/alacritty/colorize_alacritty.sh
+++ .config/alacritty/colorize_alacritty.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 # Update alacritty config to apply Xresources color scheme
-# source: https://www.reddit.com/r/unixporn/comments/8giij5/guide_defining_program_colors_through_xresources/dyfg9s3/
+# original source: https://www.reddit.com/r/unixporn/comments/8giij5/guide_defining_program_colors_through_xresources/dyfg9s3/
 
-# Target file
-target_file="$HOME/.config/alacritty/alacritty.yml"
+fill_indexed_placeholder() {
+  local color
+  color=$(getcolor "$1")
+  sed -i "s/%cl${1}%/'${color}'/g" "$2"
+}
 
-# copy input file to temporary file for black magic fuckery
-# (alacritty applies colors when the config file is written, so we want to do it
-# all in one write)
-cp "$target_file".in "$target_file".tmp
+fill_placeholders() {
+  # Numbered colors
+  for i in {0..15}
+  do
+    fill_indexed_placeholder "$i" "$1"
+  done
 
-# Grab colors from Xresources
-#xrdb ~/.Xresources
+  # Named colors
+  sed -i "s/%clfg%/'$(getcolor foreground)'/g" "$1"
+  sed -i "s/%clbg%/'$(getcolor background)'/g" "$1"
+}
 
-# Numbered colors
-for i in {0..15}
-do
-    color=$(getcolor "$i")
-    sed -i "s/%cl${i}%/'${color}'/g" "$target_file".tmp
-done
+template="${XDG_CONFIG_HOME:-"$HOME/.config"}"/alacritty/colors.yml
+tmp_file=/tmp/alacritty.tmp
+target_file="${XDG_CONFIG_HOME:-"$HOME/.config"}"/alacritty/alacritty.yml
 
-# Named colors
-foreground=$(getcolor foreground)
-background=$(getcolor background)
-sed -i "s/%clfg%/'${foreground}'/g" "$target_file".tmp
-sed -i "s/%clbg%/'${background}'/g" "$target_file".tmp
+cp "$template" "$tmp_file"
 
-# Finally, replace target file with our updated one
-rm "$target_file"
-mv "$target_file".tmp "$target_file"
+fill_placeholders "$tmp_file"
+
+cat \
+  "${XDG_CONFIG_HOME:-"$HOME/.config"}"/alacritty/settings.yml \
+  "$tmp_file" > "$target_file"
+
+rm "$tmp_file"
