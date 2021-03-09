00664387ac7e7e5709963f5801bb29d80543bbad
Felipe Dioguardi <felipedioguardi@gmail.com>
2021-03-09 11:02:13 -0300

    refactor(bspwm): remove unneeded variables

--- .config/bspwm/config/color_setup.sh
+++ .config/bspwm/config/color_setup.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-nb_color="BLACK"
-ac_color="MAGENTA"
-fb_color="BLUE"
-pb_color="GREEN"
-
-bspc config normal_border_color "$(getcolor $nb_color)"
-bspc config active_border_color "$(getcolor $ac_color)"
-bspc config focused_border_color "$(getcolor $fb_color)"
-bspc config presel_feedback_color "$(getcolor $pb_color)"
+bspc config normal_border_color "$(getcolor 'BLACK')"
+bspc config active_border_color "$(getcolor 'MAGENTA')"
+bspc config focused_border_color "$(getcolor 'BLUE')"
+bspc config presel_feedback_color "$(getcolor 'GREEN')"
