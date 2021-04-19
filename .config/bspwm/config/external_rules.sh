d70ea1964e1c0353a92fe021f23444bbf8cc21fa
Felipe Dioguardi <felipedioguardi@gmail.com>
2021-04-19 10:12:57 -0300

    refactor(bspwm): generalize rules on terminals

--- .config/bspwm/config/external_rules.sh
+++ .config/bspwm/config/external_rules.sh
@@ -3,6 +3,6 @@
 window_id=$1
 window_class=$2
 
-[ "$window_class" = "Alacritty" ] \
+[ "$window_class" = "$TERMINAL" ] \
   && [ $(xtitle "$window_id") = "bluetoothctl" ] \
-  && echo "desktop=^10 state=floating rectangle=1378x79+15+947"
+  && echo "desktop=^10 state=floating follow=off rectangle=1378x79+15+947"
