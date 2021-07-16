b5fefea2d3c0eef0b95c42acb4f9b2b78a5daa1f
Felipe Dioguardi <felipedioguardi@gmail.com>
2021-07-16 20:24:21 -0300

    feat(bspwm): add flag to change the active monitor
    When calling the script with "--use-external", disable primary monitor
    and use only the external one.

--- .config/bspwm/config/monitors_setup.sh
+++ .config/bspwm/config/monitors_setup.sh
@@ -3,8 +3,7 @@
 # Note: This is a very simplistic approach because I alerady
 #   know the settings I want to apply
 
-setup_display()
-{
+setup_display() {
   xrandr --output "$1" \
     --primary --mode 1920x1080 \
     --output "$2" \
@@ -13,42 +12,49 @@ setup_display()
     --right-of "$1"
 }
 
-one_monitor_settings()
-{
+one_monitor_settings() {
   bspc monitor "$1" -d 1 2 3 4 5 6 7 8 9 10
 }
 
-start_apps()
-{
+start_apps() {
   "$config"/polybar/launch.sh &
 }
 
-two_monitor_settings()
-{
+two_monitor_settings() {
   setup_display "$1" "$2"
 
   bspc monitor "$1" -d 1 2 3 4 5
   bspc monitor "$2" -d 6 7 8 9 10
 
   [ -x "$config"/bspwm/config/desktop_setup.sh ] \
-    && "$config"/bspwm/config/desktop_setup.sh   \
+    && "$config"/bspwm/config/desktop_setup.sh \
       --monitor "$2" &
 
   start_apps
 }
 
+only_external_monitor_settings() {
+  xrandr --output "$1" --off --output "$2" --auto
+  one_monitor_settings "$2"
+  bspc monitor "$1" --remove
+}
+
 readonly config=${XDG_CONFIG_HOME:-${HOME}/.config}
-readonly primary_monitor_name=$(xrandr --listactivemonitors \
+readonly primary=$(xrandr --listactivemonitors \
   | grep "eDP" \
   | awk 'NF {print $NF}')
 
 if [ "$(xrandr -q | grep -wc connected)" -eq 2 ]; then
-  readonly external_monitor_name=$(xrandr -q \
+  readonly external=$(xrandr -q \
     | grep -w connected \
-    | grep -v $primary_monitor_name \
+    | grep -v "$primary" \
     | cut -f1 -d" ")
 
-  two_monitor_settings $primary_monitor_name $external_monitor_name
+  if [ "$1" = "--use-external" ]; then
+    only_external_monitor_settings "$primary" "$external"
+  else
+    two_monitor_settings "$primary" "$external"
+  fi
 else
-  one_monitor_settings $primary_monitor_name
+  one_monitor_settings "$primary"
 fi
