3e723e9e394eccced1bf5fabf2c8ede423d41ec0
Felipe Dioguardi <felipedioguardi@gmail.com>
2021-07-19 13:06:50 -0300

    fix(zsh): ignore grep errors

--- .local/share/zsh/plugins/abbr/abbr.plugin.zsh
+++ .local/share/zsh/plugins/abbr/abbr.plugin.zsh
@@ -8,16 +8,17 @@ _abbreviations=()
 
 # write a function for adding an alias to the list mentioned above
 function abbr() {
-  for abbreviation in $@; do
-    alias $abbreviation
-    _abbreviations+=(${abbreviation%%\=*})
+  for abbreviation in "$@"; do
+    alias "$abbreviation"
+    _abbreviations+=("${abbreviation%%\=*}")
   done
 }
 
 # expand any aliases in the current line buffer
 function expand-abbreviation() {
   set -o extendedglob
-  if echo "|"${(j:|:)_abbreviations}"|" | grep -q "|${LBUFFER##[[:blank:]]##}|"; then
+  if echo "⬛""${(j:⬛:)_abbreviations}""⬛" \
+    | grep -q "⬛${LBUFFER##[[:blank:]]##}⬛" 2> /dev/null; then
     zle _expand_alias
     zle expand-word
   fi
