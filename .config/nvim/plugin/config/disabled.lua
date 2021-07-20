77ffadaa3467b781e795d48c3b7e9d93426bd2bf
Felipe Dioguardi <felipedioguardi@gmail.com>
2021-07-19 21:01:47 -0300

    chore(vim): disable unneeded/unused plugins

--- .config/nvim/plugin/config/disabled.lua
+++ .config/nvim/plugin/config/disabled.lua
@@ -12,6 +12,8 @@ vim.g.netrw_dirhistmax = 0
 
 -- disable standard plugins
 for _, plugin in ipairs({
-  "gzip", "loaded_2html_plugin", "matchit", "remote_plugins", "tar",
-  "tarPlugin", "zip", "zipPlugin",
+  "2html_plugin", "getscript", "getscriptPlugin", "gzip", "loaded_2html_plugin",
+  "logipat", "matchit", "netrwFileHandlers", "netrwPlugin", "netrwSettings",
+  "remote_plugins", "rrhelper", "spellfile_plugin", "tar", "tarPlugin",
+  "vimball", "vimballPlugin", "zip", "zipPlugin",
 }) do vim.g["loaded_" .. plugin] = 1 end
