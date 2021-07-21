a284b519e09c87f30c7b6542ecefd82eb9c5d8d0
Felipe Dioguardi <felipedioguardi@gmail.com>
2021-07-21 18:50:56 -0300

    fix(vim): enable netrw

--- .config/nvim/lua/disabled_.lua
+++ .config/nvim/lua/disabled_.lua
@@ -13,7 +13,7 @@ vim.g.netrw_dirhistmax = 0
 -- disable standard plugins
 for _, plugin in ipairs({
   "2html_plugin", "getscript", "getscriptPlugin", "gzip", "2html_plugin",
-  "logipat", "matchit", "netrwFileHandlers", "netrwPlugin", "netrwSettings",
+  "logipat", "matchit", "netrwFileHandlers", "netrwSettings",
   "remote_plugins", "rplugin", "rrhelper", "shada_plugin", "spec", "spellfile_plugin",
   "tar", "tarPlugin", "vimball", "vimballPlugin", "zip", "zipPlugin",
 }) do vim.g["loaded_" .. plugin] = 1 end
