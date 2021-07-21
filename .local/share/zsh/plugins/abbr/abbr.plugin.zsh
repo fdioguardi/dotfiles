cb42a98fd47e32d9f6022b52e4b05fd7fad077d0
Felipe Dioguardi <felipedioguardi@gmail.com>
2021-07-21 20:29:02 -0300

    style(zsh): capitalize comments

--- .local/share/zsh/plugins/abbr/abbr.plugin.zsh
+++ .local/share/zsh/plugins/abbr/abbr.plugin.zsh
@@ -2,11 +2,11 @@
 # abbr.zsh
 #
 
-# declare a list of expandable aliases to fill up later
+# Declare a list of expandable aliases to fill up later
 typeset -a _abbreviations
 _abbreviations=()
 
-# write a function for adding an alias to the list mentioned above
+# Write a function for adding an alias to the list mentioned above
 function abbr() {
   for abbreviation in "$@"; do
     alias "$abbreviation"
@@ -14,7 +14,7 @@ function abbr() {
   done
 }
 
-# expand any aliases in the current line buffer
+# Expand any aliases in the current line buffer
 function expand-abbreviation() {
   set -o extendedglob
   if echo "⬛""${(j:⬛:)_abbreviations}""⬛" \
@@ -29,10 +29,11 @@ zle -N expand-abbreviation
 # Bind the space key to the expand-abbreviation function above, so that space
 # will expand any expandable aliases
 bindkey ' ' expand-abbreviation
-bindkey '^ ' magic-space     # control-space to bypass completion
+bindkey '^ ' magic-space               # control-space to bypass completion
 bindkey -M isearch " " magic-space     # normal space during searches
 
-# A function for expanding any aliases before accepting the line as is and executing the entered command
+# A function for expanding any aliases before accepting the line as is and
+# executing the entered command
 expand-abbreviation-and-accept-line() {
   expand-abbreviation
   zle .backward-delete-char
