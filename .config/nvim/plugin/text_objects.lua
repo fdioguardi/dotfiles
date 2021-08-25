6e8996f4d1dff77ea5187f9282ad860b13113beb
Felipe Dioguardi <felipedioguardi@gmail.com>
2021-08-25 09:16:17 -0300

    refactor(vim): extract variable for characters

--- .config/nvim/plugin/text_objects.lua
+++ .config/nvim/plugin/text_objects.lua
@@ -4,7 +4,10 @@
 
 local M = require("mappings_.keymaps")
 
-for _, char in ipairs({ "_", ",", "/", "." }) do
+local characters = { "$", ",", ".", "/", "_" }
+
+-- setup text objects
+for _, char in ipairs(characters) do
   M.xnoremap("i" .. char, ":<C-u>normal! T" .. char .. "vt" .. char .. "<CR>")
   M.onoremap("i" .. char, ":normal vi" .. char .. "<CR>")
   M.xnoremap("a" .. char, ":<C-u>normal! F" .. char .. "vf" .. char .. "<CR>")
