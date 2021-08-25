b633774ceef963898084ef659896b41baa5465a8
Felipe Dioguardi <felipedioguardi@gmail.com>
2021-08-25 09:28:20 -0300

    feat(vim): run pre/post hooks when setting colors

--- .config/nvim/lua/plugins_/theme.lua
+++ .config/nvim/lua/plugins_/theme.lua
@@ -2,30 +2,29 @@
 -- theme.lua
 --
 
-local M = {}
+local pre_hook = {
+  gruvbox = function()
+    vim.g.gruvbox_italic = true
+    vim.g.gruvbox_sign_column = "bg0"
+    vim.g.gruvbox_contrast_dark = "hard"
+    vim.g.gruvbox_invert_selection = false
+  end,
+}
 
-function M.pre_gruvbox()
-  vim.g.gruvbox_italic = true
-
-  vim.g.gruvbox_sign_column = "bg0"
-
-  vim.g.gruvbox_contrast_dark = "hard"
-
-  vim.g.gruvbox_invert_selection = false
-end
-
-function M.post_monokai()
-  vim.cmd("highlight LineNr       guibg=none")
-  vim.cmd("highlight CursorLineNr guibg=none")
-  vim.cmd("highlight SignColumn   guibg=none")
-end
+local post_hook = {
+  monokai = function()
+    for _, group in ipairs({ "CursorLineNr", "LineNr", "Normal", "SignColumn" }) do
+      vim.cmd("highlight " .. group .. " guibg=none")
+    end
+  end,
+}
 
 return setmetatable({}, {
   __index = function(self, key)
     return function()
-      pcall(M["pre_" .. key])
+      pcall(pre_hook[key])
       vim.cmd("colorscheme " .. key)
-      pcall(M["post_" .. key])
+      pcall(post_hook[key])
     end
   end,
 })
