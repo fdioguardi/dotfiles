d331648100cf5f25f9ca5e45a5d844632a0d36d8
Felipe Dioguardi <felipedioguardi@gmail.com>
2021-11-06 16:50:27 -0300

    fix(vim): remove binaries instead of source code

--- .local/share/nvim/install_nvim.sh
+++ .local/share/nvim/install_nvim.sh
@@ -15,37 +15,39 @@ remote="https://github.com/neovim/neovim.git"
 branch="master"
 
 usage() {
-  echo "Usage: ./$0 [-r]" >&2
+  echo "Usage: $0 [-r]"
+  echo "  -r: reinstall"
+  exit "$1"
 }
 
 build_nvim() {
   make -C "$source_location" \
+    -j "$(nproc)" \
     CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$bin_location" \
     CMAKE_BUILD_TYPE="RelWithDebInfo" install
 }
 
-[ $# -gt 1 ] && usage && exit 22
+[ $# -gt 1 ] && usage 22
 
 if [ $# -eq 1 ]; then
   if [ "$1" = "-r" ] || [ "$1" = "--reinstall" ]; then
     printf "neovim: Are you sure you want to remove %s? [y/N] " "$source_location"
     read -r delete_confirmation
     if [ "$delete_confirmation" = "y" ]; then
-      rm -rf "$source_location" \
-        && echo "neovim: $source_location has been deleted"
+      rm -rf "$bin_location" \
+        && echo "neovim: $bin_location has been deleted"
     else
       echo "neovim: exiting"
       exit
     fi
   else
-    usage && exit 22
+    usage 22
   fi
 fi
 
 if [ ! -d "$source_location" ]; then
   git clone -b "$branch" --single-branch "$remote" "$source_location" \
     && build_nvim
-
   exit 0
 fi
 
@@ -63,6 +65,7 @@ fi
   git fetch
   if [ "$(git rev-parse HEAD)" = "$(git rev-parse '@{u}')" ]; then
     echo "neovim: HEAD and upstream identical"
+    [ -d "$bin_location" ] || build_nvim
   else
     echo "neovim: HEAD and upstream different"
     echo "neovim: building.."
