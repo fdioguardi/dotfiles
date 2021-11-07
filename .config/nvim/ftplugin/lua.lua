c7134ef0b0817642b05dde50811e3246717cee4e
Felipe Dioguardi <felipedioguardi@gmail.com>
2021-11-06 21:51:08 -0300

    feat(vim): add cmp's completion source for lua

--- .config/nvim/ftplugin/lua.lua
+++ .config/nvim/ftplugin/lua.lua
@@ -7,3 +7,8 @@ vim.opt_local.expandtab = true
 vim.opt_local.shiftwidth = 2
 vim.opt_local.textwidth = 80
 vim.opt_local.wrap = false
+
+-- enable completion source
+require("cmp").setup.buffer({
+  sources = { { name = "nvim_lua" }, unpack(require("plugins_.cmp").sources) },
+})
