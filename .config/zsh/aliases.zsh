6846d095e127432d7afdf49cbc8d7b3ec8768e8a
Felipe Dioguardi <felipedioguardi@gmail.com>
2021-11-06 21:11:22 -0300

    refactor(zsh): use double quotes and escape chars

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -3,43 +3,43 @@
 #
 
 alias \
-  grep='grep --color=auto' \
-  ls='ls --color'
+  grep="grep --color=auto" \
+  ls="ls --color"
 
 abbr \
-  bat='bat --decorations=never' \
-  chx='chmod +x' \
-  f='find . -name' \
-  ff='sudo find / -name' \
-  l='ls -A --group-directories-first' \
-  ll='ls -Agho --group-directories-first' \
-  tmp='cd $(mktemp -d)' \
-  rmd='rmdir' \
-  mkd='mkdir -pv' \
-  ...='cd ../../' \
-  ....='cd ../../../' \
-  .....='cd ../../../../'
+  bat="bat --decorations=never" \
+  chx="chmod +x" \
+  f="find . -name" \
+  ff="sudo find / -name" \
+  l="ls -A --group-directories-first" \
+  ll="ls -Agho --group-directories-first" \
+  tmp="cd \"\$(mktemp -d)\"" \
+  rmd="rmdir" \
+  mkd="mkdir -pv" \
+  ...="cd ../../" \
+  ....="cd ../../../" \
+  .....="cd ../../../../"
 
 abbr \
-  p='paru' \
-  i='yes "" | paru -S --needed' \
-  r='paru -Rns' \
-  ro='paru --clean --noconfirm && topgrade -yc' \
-  u='topgrade -y &; "$XDG_DATA_HOME"/nvim/install_nvim.sh &' \
-  gpip='PIP_REQUIRE_VIRTUALENV=0 pip'
+  p="paru" \
+  i="yes "" | paru -S --needed" \
+  r="paru -Rns" \
+  ro="paru --clean --noconfirm && yes | topgrade -c" \
+  u="topgrade -y &; \"\$XDG_DATA_HOME\"/nvim/install_nvim.sh &" \
+  gpip="PIP_REQUIRE_VIRTUALENV=0 pip"
 
 abbr \
-  calc='noglob math' \
-  cu='nmcli connection up' \
-  g='git' \
-  ka='killall' \
-  pls='sudo $(fc -ln -1)' \
-  rc='recolorize' \
-  venv='source .venv/bin/activate'
+  calc="noglob math" \
+  cu="nmcli connection up" \
+  g="git" \
+  ka="killall" \
+  pls="sudo \"\$(fc -ln -1)\"" \
+  rc="recolorize" \
+  venv="source .venv/bin/activate"
 
-alias vi='v' vim='v'
+alias vi="v" vim="v"
 
 alias \
-  battery='cat /sys/class/power_supply/BAT0/uevent' \
-  brightness='xrandr --output $(xrandr --query | grep "primary" | cut -f1 -d" ") --brightness' \
-  d='/usr/bin/git --git-dir="$HOME"/.dotfiles --work-tree="$HOME"' # Managing dotfiles
+  battery="cat /sys/class/power_supply/BAT0/uevent" \
+  brightness="xrandr --output \"\$(xrandr --query | grep primary | cut -f1 -d' ')\" --brightness" \
+  d="/usr/bin/git --git-dir=\"\$HOME/.dotfiles\" --work-tree=\"\$HOME\"" # Managing dotfiles
--- .config/zsh/completion.zsh
+++ .config/zsh/completion.zsh
@@ -6,5 +6,5 @@ autoload -Uz compinit
 zstyle ':completion:*' menu select
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 zmodload zsh/complist
-compinit -d ${XDG_CACHE_HOME:-${HOME}/.cache}/zsh/zcompdump
+compinit -d "${XDG_CACHE_HOME:-${HOME}/.cache}/zsh/zcompdump"
 _comp_options+=(globdots)
