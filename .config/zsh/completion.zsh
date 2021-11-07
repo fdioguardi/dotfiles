fa1861c1f45b3040edf924eaf154dbea646979eb
Felipe Dioguardi <felipedioguardi@gmail.com>
2021-11-06 21:12:36 -0300

    feat(zsh): expand aliases on tab completion

--- .config/zsh/completion.zsh
+++ .config/zsh/completion.zsh
@@ -4,6 +4,7 @@
 
 autoload -Uz compinit
 zstyle ':completion:*' menu select
+zstyle ':completion:*' completer _expand_alias _complete _ignored
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 zmodload zsh/complist
 compinit -d "${XDG_CACHE_HOME:-${HOME}/.cache}/zsh/zcompdump"
