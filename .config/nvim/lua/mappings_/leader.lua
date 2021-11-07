12fb261c487422832094875cf627923da736e4ea
Felipe Dioguardi <felipedioguardi@gmail.com>
2021-11-06 21:47:01 -0300

    refactor(vim): use anonymous function

--- .config/nvim/lua/mappings_/leader.lua
+++ .config/nvim/lua/mappings_/leader.lua
@@ -2,23 +2,21 @@
 -- leader.lua
 --
 
-local __cycle_numbers = {
-  ["00"] = function()
-    vim.opt.nu, vim.opt.rnu = true, true
-  end,
-  ["01"] = function()
-    vim.opt.nu, vim.opt.rnu = true, false
-  end,
-  ["10"] = function()
-    vim.opt.nu, vim.opt.rnu = false, false
-  end,
-  ["11"] = function()
-    vim.opt.nu, vim.opt.rnu = true, false
-  end,
-}
-
 return {
   cycle_numbers = function()
-    __cycle_numbers[(vim.wo.nu and "1" or "0") .. (vim.wo.rnu and "1" or "0")]()
+    ({
+      ["00"] = function()
+        vim.opt.nu, vim.opt.rnu = true, true
+      end,
+      ["01"] = function()
+        vim.opt.nu, vim.opt.rnu = true, false
+      end,
+      ["10"] = function()
+        vim.opt.nu, vim.opt.rnu = false, false
+      end,
+      ["11"] = function()
+        vim.opt.nu, vim.opt.rnu = true, false
+      end,
+    })[(vim.wo.nu and "1" or "0") .. (vim.wo.rnu and "1" or "0")]()
   end,
 }
